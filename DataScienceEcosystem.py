0950083d48870352e49c47e381146da1d6742b9f
dangthai1912 <159222088+dangthai1912@users.noreply.github.com>
2024-02-07 11:18:34 +0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized
+
+# #### Some of the popular languages that Data Scientists use are
+
+# In[1]:
+
+
+languages = ["Python", "SQL", "Rstudio"]
+
+
+# In[ ]:
+
+
+
+
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+
+# In[2]:
+
+
+lib = ["numpy", "pandas", "TensorFlow"]
+
+
+# In[ ]:
+
+
+
+
+
+# #### Data Science Tools
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+df = pd.DataFrame(lib, columns=['Data Science Tools'])
+
+
+# In[6]:
+
+
+df
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[10]:
+
+
+a = (3*4)+5
+
+
+# In[14]:
+
+
+print("This a simple arithmetic expression to mutiply then add integers " + str(a))
+
+
+# In[ ]:
+
+
+
+
+
+# #### convert minutes to hours
+
+# In[15]:
+
+
+m = 200
+h = round(m/60,1)
+
+
+# In[16]:
+
+
+print("This will convert 200 minutes to hours by diving by 60 " + str(h))
+
+
+# In[ ]:
+
+
+
+
+
+# #### Objectives:
+
+# In[ ]:
+
+
+
+
+
+# ## Thai author
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
